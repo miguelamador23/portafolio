e81301aaa7c938d9abf0095cbdb92d9451ee1b12
¨miguelamador23¨ <miguelamador23@outlook.com>
2024-01-16 11:20:58 -0600

    Version1

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import { useState } from 'react'
+import Nav from './components/Nav'
+
+function App() {
+
+
+  return (
+    <>
+      <Nav />
+    </>
+  )
+}
+
+export default App
